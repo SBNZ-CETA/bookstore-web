641336ecd09d7c78e3e5f25dd02b6337fc5008c8
Lee <licina.luka@gmail.com>
2023-05-24 22:19:50 +0200

    JWT-Fix/Todays Checkpoint

--- src/app/utils/tocken.interceptor.ts
+++ src/app/utils/tocken.interceptor.ts
@@ -7,10 +7,21 @@ import { catchError, Observable, throwError } from "rxjs";
 export class TokenInterceptor implements HttpInterceptor { 
     constructor(private router: Router) { } 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        var token = localStorage.getItem('token')
+        if(token == null){
+            return next.handle(request).
+                pipe(catchError(err => { 
+            if (err.status === 401) {
+            localStorage.removeItem('token');      
+            this.router.navigate(['/login']);} 
+            const error = err.error.message || err.statusText; 
+            return throwError(error); 
+            }))
+        }
         const cloned = request.clone(
             {
                 setHeaders: {  
-                    Authorization: `Bearer ${localStorage.getItem('token')}`  
+                    Authorization: `Bearer ` + token  
                 }  
             }
         );
